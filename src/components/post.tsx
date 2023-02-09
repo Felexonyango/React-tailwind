33b0b3c091508dff8926ad5728ded4a7828b8306
Felexonyango <felexonyango19@gmail.com>
2023-02-09 13:18:30 +0300

    added react-query

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import PostList from "./PostList";
+import { useQuery } from "@tanstack/react-query";
+
 interface IPosts {
   body: string;
   title: string;
@@ -10,21 +12,22 @@ const RenderPostList = (props: { body: string; title: string }) => {
 };
 
 const Post: React.FC<IPosts> = () => {
-  const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    const getposts = async () => {
-      const result = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
-      setPosts(result.data);
-    };
-    getposts();
-  }, []);
+  function getposts(){
+   return fetch('https://jsonplaceholder.typicode.com/posts')
+   .then((res)=>res.json())
+   .catch((err)=>console.log(err))
+  };
+  const { isLoading, isError, data, error } = useQuery(['posts'], getposts)
 
+  if(isError) return <h1>{JSON.stringify(error)}</h1>
+  if(isLoading) return<h1>isLoading...</h1>
+  console.log(data)
+
+ 
   return (
     <div>
-      {posts.map((post: any) => (
+      {data.map((post:any) => (
         <ul key={post.id}>
           <RenderPostList body={post.body} title={post.title} />
         </ul>
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { QueryClient,QueryClientProvider } from '@tanstack/react-query'
+
+const queryclient= new QueryClient(
+  //options
+)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryclient}>
     <App />
+
+    </QueryClientProvider>
   </React.StrictMode>,
 )
